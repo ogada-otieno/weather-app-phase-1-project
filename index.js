14baf11567ee74c5daa31ebf9ef275947d6b0c64
Rodgers Ogada <ogadarodgers@gmail.com>
2022-12-15 11:19:52 +0300

    added event listener for keypress in the search box

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let apiKey = "651d508623e4eb736ff502f4b3410f7a";
 
 const searchbox = document.querySelector('#searchCity');
 
+searchbox.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    weatherData(searchbox.value);
+  }
+});
