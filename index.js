db2aa6d0c5fa2ef8fd42ebd92c40f236675d756a
Rodgers Ogada <ogadarodgers@gmail.com>
2022-12-15 11:24:21 +0300

    defined destinations appended content to the same

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ function weatherData(city) {
     .then((resp) => resp.json()) // converts to json format
     .then(insertDetails)
 }
+
+
+// define destinations and send data to the destinations
+function insertDetails(data) {
+  document.getElementById(
+    "city"
+  ).innerText = `${data.name}, ${data.sys.country}`;
+  document.getElementById("date").innerText = "Wednesday, December 14, 2022";
+
+  let temperature = Math.round(data.main.temp);
+  document.getElementById("temp").innerText = `${temperature} °C`;
+
+  document.getElementById("description").innerText =
+    data.weather[0].description;
+
+  let minTemp = Math.round(data.main.temp_min);
+  let maxTemp = Math.round(data.main.temp_max);
+  document.getElementById("minMax").innerText = `${minTemp} °C / ${maxTemp} °C`;
+}
